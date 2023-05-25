30c33b14b82a52f5b05ce42a8ce5c49678635e26
Hoang Nhan <hnhan793@gmail.com>
2023-05-25 23:44:18 +0700

    done

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,15 @@
 const groceryInput = document.querySelector('.grocery__input');
-const groceryBtn = document.querySelector('.grocery__btn');
-const groceryTable = document.querySelector('.grocery__table');
+const submitBtn = document.querySelector('.submit__btn');
+const editBtn = document.querySelector('.edit__btn');
 const groceryClear = document.querySelector('.grocery__clear');
+const groceryTable = document.querySelector('.grocery__table');
 const groceryForm = document.querySelector('.grocery__form');
 
 const itemList = [];
 let index = 0;
 
-function groceryList(item, index) {
+function groceryList(item) {
     this.item = item;
-    this.id = index;
 }
 
 function checkValidation() {
@@ -25,44 +25,55 @@ function showAlert(alertText, alertType) {
     }, 1000);
 }
 
-function removeItem() {
-    const delBtnList = document.querySelectorAll('.grocery__del-btn');
-    delBtnList.forEach(function (item, index) {
-        item.addEventListener('click', function (e) {
-            e.preventDefault();
-            const getElement = e.target.dataset.id;
-            document.getElementById(getElement).outerHTML = '';
-            itemList.splice(index, 1);
-            showAlert('Item Removed', 'danger');
-            console.log(index, itemList);
-        })
+function displayItem() {
+    let content = '';
+    groceryTable.innerHTML = '';
+
+    content += itemList.map(function (item, index) {
+        return `<li class="flex justify-between items-center text-slate-800 hover:text-slate-400 hover:bg-slate-100 rounded-lg px-4 py-1 transition duration-500 my-1" >
+                    <p class="tracking-widest">${item.item}</p>
+                    <div class="flex text-xs">
+                        <button onclick="editItem(${index})" class="grocery__edit-btn text-green-400">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </button>
+                        <button onclick="removeItem(${index})" class="grocery__del-btn ml-4 text-red-500">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>   
+                    </div>
+                </li>`
+    }).join('');
+    groceryTable.innerHTML = content;
+}
+
+function removeItem(id) {
+    itemList.splice(id, 1);
+    displayItem();
+    showAlert('Item removed', 'danger');
+}
+
+function editItem(id) {
+    submitBtn.style.display = 'none';
+    editBtn.style.display = 'block';
+
+    groceryInput.value = itemList[id].item;
+    editBtn.addEventListener('click', function () {
+        itemList[id].item = groceryInput.value;
+        displayItem();
+        submitBtn.style.display = 'block';
+        editBtn.style.display = 'none';
+        groceryForm.reset();
     })
 }
 
 function addItem() {
     let item = groceryInput.value;
-    itemList[index] = new groceryList(item, index);
-    let element = document.createElement('li');
-    element.id = `liIndex-${itemList[index].id}`;
-    element.classList = 'flex justify-between items-center text-slate-800 hover:text-slate-400 hover:bg-slate-100 rounded-lg px-4 py-1 transition duration-500 my-1';
-    element.innerHTML = `
-        <p class="tracking-widest">${itemList[index].item}</p>
-        <div class="flex text-xs">
-            <a href="" data-id="liIndex-${itemList[index].id}" class="block grocery__edit-btn text-green-400">
-                <i class="fa-solid fa-pen-to-square"></i>
-            </a>
-            <a href="" class="block grocery__del-btn ml-4 text-red-500">
-                <i data-id="liIndex-${itemList[index].id}" class="fa-solid fa-trash"></i>
-            </a>
-        </div>
-    `;
+    itemList[index] = new groceryList(item);
     index++;
-    showAlert('Item added', 'success');
 
-    groceryTable.appendChild(element);
     groceryTable.classList.add('active');
     groceryClear.classList.add('active');
     groceryForm.reset();
+    showAlert('Item added', 'success');
 }
 
 function clearItem() {
@@ -74,10 +85,10 @@ function clearItem() {
     showAlert('Empty List', 'danger');
 }
 
-groceryBtn.addEventListener('click', function () {
+submitBtn.addEventListener('click', function () {
     if (checkValidation()) {
         addItem();
-        removeItem();
+        displayItem();
     }
     else {
         showAlert('Please Enter Value', 'danger');
